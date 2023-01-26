9510bb291accc651aba828f3bba2320c7765e480
James Lee Lendrem <J.l.lendrem@smoothlines.net>
2023-01-26 22:40:02 +0000

    Add area for number, scores. The rects look blurry to me.

--- src/commonMain/kotlin/main.kt
+++ src/commonMain/kotlin/main.kt
@@ -11,7 +11,7 @@ import com.soywiz.korma.geom.*
 import com.soywiz.korma.geom.vector.*
 import com.soywiz.korma.interpolation.*
 
-
+// Remember we are using: https://lospec.com/palette-list/maseclas-basic
 
 
 suspend fun main() = Korge(width = 512, height = 512, bgcolor = Colors["#2b2b2b"]) {
@@ -41,12 +41,35 @@ class MyScene : Scene() {
         val cellWidth = (gridWidth - ((columnCount + 1) * guideLine)) / columnCount;
         val cellHeight = (gridHeight - ((rowCount + 1) * guideLine)) / rowCount;
 
-        fixedSizeContainer(stage.width, stage.height) {
+        fixedSizeContainer(stage.width, topIndent) {
+            x = 0.0;
+            y = 0.0;
+            roundRect(stage.width, topIndent, 4.0, fill = Colors["#212339"]) {
+                x = 0.0;
+                y = 0.0;
+            }
+            roundRect(cellWidth * 2 + guideLine, topIndent - (guideLine * 1), 5.0, fill = Colors["#82c9b9"]) {
+                x = guideLine;
+                y = guideLine;
+            }
+            for (i in 0..1) {
+                roundRect(cellWidth * 2 + guideLine, (topIndent / 2) - guideLine, 5.0, fill = Colors["#82c9b9"]) {
+                    x = guideLine + ((guideLine + cellWidth) * 2);
+                    y = guideLine + ((topIndent / 2) * i);
+                }
+            }
+        }
+
+        fixedSizeContainer(gridWidth, gridHeight) {
             x = 0.0;
             y = topIndent;
+            roundRect(gridWidth, gridHeight, 4.0, fill = Colors["#212339"]) {
+                x = 0.0;
+                y = 0.0;
+            }
             for (i in 0..3) {
                 for (j in 0..3) {
-                    roundRect(cellWidth, cellHeight, 5.0, fill = Colors["#b9aea0"]) {
+                    roundRect(cellWidth, cellHeight, 5.0, fill = Colors["#82c9b9"]) {
                         x = guideLine + (guideLine + cellWidth) * i;
                         y = guideLine + (guideLine + cellHeight) * j;
                     }
