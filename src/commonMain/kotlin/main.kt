60ae401dfcb8e6e626043d44ffa7154e6608f70c
James Lee Lendrem <J.l.lendrem@smoothlines.net>
2023-01-26 22:06:14 +0000

    clean up commits

--- src/commonMain/kotlin/main.kt
+++ src/commonMain/kotlin/main.kt
@@ -17,14 +17,8 @@ import com.soywiz.korma.interpolation.*
 suspend fun main() = Korge(width = 512, height = 512, bgcolor = Colors["#2b2b2b"]) {
 	val sceneContainer = sceneContainer();
 
-
-    //bgField.x = leftIndent;
-    //bgField.y = topIndent;
-
-    //addChild(bgField);
-
 	sceneContainer.changeTo({ MyScene() })
-    //Test
+
 }
 
 class MyScene : Scene() {
@@ -33,15 +27,6 @@ class MyScene : Scene() {
 		val minDegrees = (-16).degrees
 		val maxDegrees = (+16).degrees
         val stage = stage!!
-
-        //val cellSize = views.virtualWidth / 5.0;
-
-        //println("CellSize  $cellSize");
-        println("Virtual Width  ${views.virtualWidth}");
-
-        //What? I have no idea what this is for...
-        //vl fieldSize = 50.0 * 4 * cellSize;a
-        //val leftIndent = (views.virtualWidth - fieldSize) / 2;
 
         //Works
         val topIndent = 150.0
@@ -56,17 +41,6 @@ class MyScene : Scene() {
         val cellWidth = (gridWidth - ((columnCount + 1) * guideLine)) / columnCount;
         val cellHeight = (gridHeight - ((rowCount + 1) * guideLine)) / rowCount;
 
-
-
-
-        //Doesn't work
-        //val topIndent: Double = 150;
-        //New languages are fucking insane.
-
-        /*val bgField = roundRect(fieldSize, fieldSize, 5.0, fill = Colors["#b9aea0"]) {
-            x = leftIndent;
-            y = topIndent;
-        };*/
         fixedSizeContainer(stage.width, stage.height) {
             x = 0.0;
             y = topIndent;
@@ -80,31 +54,5 @@ class MyScene : Scene() {
             }
         }
 
-        //val graphicPos = position(leftIndent, topIndent);
-
-        /*graphics {
-            it.position(leftIndent, topIndent)
-            fill(Colors["#cec0b2"]) {
-                for (i in 0..3) {
-                    for (j in 0..3) {
-                        roundRect(10.0 + (10 + cellSize) * i, 10.0 + (10 + cellSize) * j, cellSize, cellSize, 5.0)
-                    }
-                }
-            }
-        }*/
-
-        //graphics.addTo(this);
-
-		/* val image = image(resourcesVfs["korge.png"].readBitmap()) {
-			rotation = maxDegrees
-			anchor(.5, .5)
-			scale(0.8)
-			position(256, 256)
-		}
-
-		while (true) {
-			image.tween(image::rotation[minDegrees], time = 1.seconds, easing = Easing.EASE_IN_OUT)
-			image.tween(image::rotation[maxDegrees], time = 1.seconds, easing = Easing.EASE_IN_OUT)
-		} */
 	}
 }
